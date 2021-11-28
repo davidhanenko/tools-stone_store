f7e495c242e7462e3c0ea999ceb936feaa23aada
davidhanenko <d.hanenko24@gmail.com>
2021-11-28 00:28:02 -0500

    test deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,46 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false),
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+
+  if(env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DB_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password
+          },
+          options: {
+            ssl: false
+          },
+        },
       },
-      options: {}
+    };
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          username: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        options: {}
+      },
     },
-  },
-});
+  }
+};
--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,25 +1,26 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
-    },
-    breakpoints: {
-      xsmall: 320,
-    },
-    actionOptions: {
-      upload: {
-         transformation: {
-             "width": 1920
-          },
-        fetch_format: 'auto',
-        quality: 'auto',
-        resource_type: 'auto',
-        folder: 'A2Z',
+module.exports = ({ env }) => {
+  return  {
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
-      delete: {},
+      breakpoints: {
+        xsmall: 320,
+      },
+      actionOptions: {
+        upload: {
+          transformation: {
+              "width": 1920
+            },
+          fetch_format: 'auto',
+          quality: 'auto',
+          resource_type: 'auto',
+          folder: 'A2Z',
+        },
+        delete: {},
+      },
     },
-  },
-});
+}};
